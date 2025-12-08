e064589d1f5cc97e9a916eb9e4f743f6093d8b9b
Emils <emils@gmail.com>
2025-12-08 08:55:51 +0200

    inputs

--- Mortgage calculator/Main.java
+++ Mortgage calculator/Main.java
@@ -1,8 +1,18 @@
 
+import java.util.Scanner;
+
 public class Main{
 
     public static void main(String[] args){
+        Scanner myobj = new Scanner(System.in);
         System.out.println("Kredīta kalkulātors");
+
+        System.out.println("Kredīta summa -->");
+        double creditSum = myobj.nextDouble();
+        System.out.println("Gada procentu likme -->");
+        float procentLikme = myobj.nextFloat();
+        System.out.println("Atmaksas termiņš(gados) -->");
+        int termin = myobj.nextInt();
     }
 
 }
